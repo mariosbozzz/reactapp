9e2397ba5f54a9b0913510feefa35164c1464f10
unknown <mariosalma@hotmail.com>
2024-04-01 16:55:47 +0300

    make it better

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import {Sequelize} from "sequelize";
 
 const db = new Sequelize('u700861365_coach', 'u700861365_marios', '4261!Marios', {
-    host: "153.92.220.1",
+    host: "18.201.88.187",
     dialect: "mysql"
 });
 
