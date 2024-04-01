ab87b8bb88747dbcbf95c64a0fdd7bc2886411d6
unknown <mariosalma@hotmail.com>
2024-04-01 17:04:04 +0300

    make it better

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,9 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('u700861365_coach', 'u700861365_marios', '4261!Marios', {
-    host: "18.201.88.187",
+const db = new Sequelize('heroku_251b7ffcbdfdbb5', 'be2ffa22947a17', '1bc1bd62', {
+    host: "eu-cluster-west-01.k8s.cleardb.net",
     dialect: "mysql"
 });
 
 export default db;
+
